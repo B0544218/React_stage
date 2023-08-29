f7641b85f17efa8b788ba6d61588d8c385d6d808
Kai-Yuan <32070909+B0544218@users.noreply.github.com>
2023-08-29 23:45:50 +0800

    Update index.jsx

--- 15_src_嵌套路由的使用/pages/Home/Message/index.jsx
+++ 15_src_嵌套路由的使用/pages/Home/Message/index.jsx
@@ -6,13 +6,13 @@ export default class Message extends Component {
 			<div>
 				<ul>
 					<li>
-						<a href="/message1">message001</a>&nbsp;&nbsp;
+						<a href="/message1">超連結message001</a>&nbsp;&nbsp;
 					</li>
 					<li>
-						<a href="/message2">message002</a>&nbsp;&nbsp;
+						<a href="/message2">超連結message002</a>&nbsp;&nbsp;
 					</li>
 					<li>
-						<a href="/message/3">message003</a>&nbsp;&nbsp;
+						<a href="/message/3">超連結message003</a>&nbsp;&nbsp;
 					</li>
 				</ul>
 			</div>
